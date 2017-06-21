e870637615f2705362f6ed8778a967a9b0de20e3
as-shimizu <as-shimizu@teratech.co.jp>
2017-06-21 11:39:44 +0900

    #3 app.jsの入力ミスを変更

--- Chapter3/pizza/js/app.js
+++ Chapter3/pizza/js/app.js
@@ -3,13 +3,13 @@ var budget = prompt('所持金を数字で入力してください');
 budget = parseFloat(budget);
 
 //おなかのすき具合
-var is Hungry = confirm('お腹は空いていますか？');
+var isHungry = confirm('お腹は空いていますか？');
 
 //所持金が1500円以上ならピザを購入
 //＋所持金が1500未満のとき
 if (budget >= 1500 && isHungry) {
   alert('ピザを注文しました');
-} else if (budget >= 500) {
+} else if (budget >= 500 && isHungry) {
   alert('ポテトを購入しました');
 } else {
   alert('節約、節約．．．');
