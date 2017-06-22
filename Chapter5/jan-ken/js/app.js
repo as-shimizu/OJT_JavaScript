a2670c459e640cced6c06444007359f13d75b54f
as-shimizu <as-shimizu@teratech.co.jp>
2017-06-22 11:32:24 +0900

    #5 連続ジャンケン機能を追加

--- Chapter5/jan-ken/js/app.js
+++ Chapter5/jan-ken/js/app.js
@@ -1,3 +1,26 @@
+//動作
+var winlow = 0;　//勝ち負け（あいこ：１　勝ち：２　負け：３）
+var winning = 0;　//連勝回数
+
+//ジャンケンの手に番号を設定
+var GU = 1;
+var CHOKI = 2;
+var PA = 3;
+
+while ( winlow !== 3) {
+  //  ジャンケンする
+  winlow = janken();
+
+  //勝った場合は連勝を加算
+  if(winlow ===2) {
+    winning++;
+  }
+}
+
+//連勝記録
+alert("ただいまの連勝記録は" + winning + "回でした");
+
+// 関数の定義
 //手を入力する関数
 function input() {
   var hand = prompt('半角数字で1～3の数字を入力してください。\n\n' + GU + ':グー\n' + CHOKI + ':チョキ\n' + PA + ':パー');
@@ -31,44 +54,65 @@ function handName(hd) {
   }
 }
 
-//メッセージを決める
+//結果判定の関数
 function result(hand, com) {
-  var msgResult = "";
+  var winlow = 0;
 
   //あいこの場合
   if (hand === com) {
-    msgResult = "あいこでした";
-    return msgResult;
+    winlow = 1;
+    return winlow;
 
   //handが勝った場合
   } else if (com - hand ===1 || com - hand === -2) {
-    msgResult = "勝ちました";
-    return msgResult;
+    winlow = 2;
+    return winlow;
 
   //handが負けた場合
   } else {
-    msgResult = "負けました"
-    return msgResult;
+    winlow = 3;
+    return winlow;
   }
 }
 
-//ジャンケンの手に番号を設定
-var GU = 1;
-var CHOKI = 2;
-var PA = 3;
+//結果のメッセージを作成する関数
+function message(hand, com) {
+  var msgResult = "";
+  var winlow = result(hand,com);
 
-//ジャンケンの手を入力する
-var hand = input();
+  //あいこの場合
+  if ( winlow === 1) {
+    msgResult = "あいこでした";
+    return  msgResult;
 
-//コンピュータの手を決める
-var com = Math.floor(Math.random() *3) + 1;
+  //勝った場合
+  } else if (winlow === 2) {
+    msgResult = "勝ちました";
+    return  msgResult;
 
-//コンピュータの手の名前
+  //負けた場合
+  } else if (winlow === 3) {
+      msgResult = "負けました";
+      return  msgResult;
+  }
+}
+
+//ジャンケンを1回行う関数
+function janken () {
+  //ジャンケンの手を入力する
+  var hand = input();
+
+  //コンピュータの手を決める
+  var com = Math.floor(Math.random() *3) + 1;
+
+  //コンピュータの手の名前
   var hd = handName(com);
 
+  //結果判定
+  var winlow = result(hand,com);
+  var msgResult = message(hand, com);
 
-
-//結果判定
-  msgResult = result(hand, com);
-//結果の出力
+  //結果の出力
   alert(msgResult + '\n コンピュータの出した手は「' + hd + '」でした');
+  return winlow;
+}
