5df157be6d7d8d768ba061c843b84e7d49efa989
as-shimizu <as-shimizu@teratech.co.jp>
2017-06-29 15:40:27 +0900

    #11 住所検索機能追加

--- Chapter11/zip_code/js/app.js
+++ Chapter11/zip_code/js/app.js
@@ -3,16 +3,14 @@ $(function() {
   $('#search').on('click',function() {
     //ＷＥＢアプリに郵便番号をリクエスト
     $.ajax({
-      url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('zip').val(),
+      url: "http://zipcloud.ibsnet.co.jp/api/search?zipcode=" + $('#zip').val(),
       dataType : 'jsonp',
     //処理が終わったら
     }).done(function(data) {
       //console.log(data);
       if (data.results) {
         //データ取得できたとき
-        
-
-
+        setData(data.results[0]);
       }else {
         alert('該当するデータが見つかりませんでした');
       }
@@ -22,3 +20,10 @@ $(function() {
     });
   });
 });
+
+function setData(data) {
+  //住所入力
+  $('#prefecture').val(data.address1);
+  $('#city').val(data.address2);
+  $('#after').val(data.address3);
+}
