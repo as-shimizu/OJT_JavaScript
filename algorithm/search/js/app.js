a6fd3c228ff2fd177d129c09a2a7f49f9189420e
as-shimizu <as-shimizu@teratech.co.jp>
2017-07-06 11:03:19 +0900

    #15 データ読み込み方法を変更

--- algorithm/search/js/app.js
+++ algorithm/search/js/app.js
@@ -1,38 +1,53 @@
 
-var data = getCSV("odd.csv");
-
 $(function () {
-  //検索ボタンが押された時
-  $('#search').click(function() {
-    //検索値の取得
-    var textarea = document.getElementById("target").value;
-    var target = Number(textarea);
+  var selfile = document.getElementById("selfile");
+  selfile.addEventListener("change",function(evt) {
+    var file = evt.target.files;
+    var reader = new FileReader();
+    reader.readAsText(file[0]);
+    reader.onload = function(ev) {
+      var lines = reader.result.split("\n");
+      var data =[];
 
-    //線形探索で検索する
-    var startTime = new Date(); //開始時間の取得
-    var msg = linerSearch(data, target);
-    var endTime = new Date();　//終了時間の取得
-    var time = endTime.getTime() - startTime.getTime();
-    console.log(time);
+      for (var i=0; i<lines.length; i++) {
+        var column = lines[i].split(",");
+        data.push(column[0]);
+      }
 
-    //結果出力
-    var result1 = document.getElementById("result1");
-    result1.innerText = msg + '所要時間：' + time + ' (ミリ秒)';
-    //$('#result').show();
+      //検索ボタンが押された時
+      $('#search').click(function() {
+        //検索値の取得
+        var textarea = document.getElementById("target").value;
+        var target = Number(textarea);
+        console.log(target);
 
-    //二分探索で検索する
-    var startTime = new Date(); //開始時間の取得
-    var msg = binarySearch(data, target);
-    var endTime = new Date();　//終了時間の取得
-    var time = endTime.getTime() - startTime.getTime();
-    console.log(time);
+        //線形探索で検索する
+        var startTime = new Date(); //開始時間の取得
+        var msg = linerSearch(data, target);
+        var endTime = new Date();　//終了時間の取得
+        var time = endTime.getTime() - startTime.getTime();
+        console.log(time);
 
-    //結果出力
-    var result2 = document.getElementById("result2");
-    result2.innerText = msg + '所要時間：' + time + ' (ミリ秒)';
-    $('#result').show();
-  })
-})
+        //結果出力
+        var result1 = document.getElementById("result1");
+        result1.innerText = msg + '所要時間：' + time + ' (ミリ秒)';
+        //$('#result').show();
+
+        /*//二分探索で検索する
+        var startTime = new Date(); //開始時間の取得
+        var msg = binarySearch(data, target);
+        var endTime = new Date();　//終了時間の取得
+        var time = endTime.getTime() - startTime.getTime();
+        console.log(time);
+
+        //結果出力
+        var result2 = document.getElementById("result2");
+        result2.innerText = msg + '所要時間：' + time + ' (ミリ秒)';
+        $('#result').show();*/
+      }) //（検索ボタンを押したとき）
+    } //reader.onload = function(ev) {
+  })//selfile.addEventListener("change",function(evt)
+})//$(function () 
 
 
 //（１）線形探索
@@ -100,4 +115,22 @@ function getCSV(filename) {
     data.push(column[0]);
   }
   return data;
+}
+
+//テキストファイルで読み込む
+function getData() {
+  var file = evt.target.files;
+  var reader = new FileReader();
+  reader.readAsText(file[0]);
+  reader.onload = function(ev) {
+    var lines = reader.result.split("\n");
+    var data =[];
+
+    for (var i=0; i<lines.length; i++) {
+      var column = lines[i].split(",");
+      data.push(column[0]);
+    }
+    console.log(data);
+    return data;
+  }
 }
