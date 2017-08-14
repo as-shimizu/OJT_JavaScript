f5f80e685664b831422d3e08ebcdc9e6481bbe97
as-shimizu <as-shimizu@teratech.co.jp>
2017-08-14 13:59:46 +0900

    #16 表示画面作成

--- algorithm/dice_game/js/app.js
+++ algorithm/dice_game/js/app.js
@@ -7,6 +7,7 @@ var yaku = function(name, rate) {
     this.rate  = rate;
 }
 var numDice = 3;
+var resultYaku = new yaku("",0);
 
 
 //所持金を設定
@@ -33,13 +34,13 @@ $('#dice').click(function() {
         //サイコロの画像を表示
         console.log(dice);
         //役とレートの取得
-        var result = getNameRate(dice);
+        getNameRate(dice);
 
-        var changeMoney = latch * result.rate;
+        var changeMoney = latch * resultYaku.rate;
         money += changeMoney;
 
         //HTMLに出力
-        console.log(result.name, result.rate, changeMoney,money);
+        console.log(resultYaku.name, resultYaku.rate, changeMoney,money);
     }
 });
 
@@ -94,27 +95,26 @@ function getNameRate(dice) {
 
     if(dice[0]==dice[1] && dice[0]==dice[2]) {
         if(dice[0]==1) {
-            var result = new yaku("ピンゾロ",5);
-            return result;
+            resultYaku = new yaku("ピンゾロ",5);
+            //return result;
         } else {
-            var result = new yaku("ゾロ目",3);
+            resultYaku = new yaku("ゾロ目",3);
             return result;
         }
     } else if(dice[0]==4 && dice[1]==5 && dice[2]==6) {
-        var result = new yaku("シゴロ",2);
-        return result;
+        resultYaku = new yaku("シゴロ",2);
+        //return result;
     } else if(dice[0]==dice[1]) {
-        var result = new yaku(dice[2] + "の目",1);
-        return result;
+        resultYaku = new yaku(dice[2] + "の目",1);
+        //return result;
     } else if(dice[1]==dice[2]) {
-        var result = new yaku(dice[0] + "の目",1);
-        return result;
+        resultYaku = new yaku(dice[0] + "の目",1);
+        //return result;
     } else if(dice[0]==1 && dice[1]==2 && dice[2]==3) {
-        var result = new yaku("ヒフミ",-2);
-        return result;
+        resultYaku = new yaku("ヒフミ",-2);
+        //return result;
     }
-    var result = new yaku("目なし",-1);
-    return result;
+    resultYaku = new yaku("目なし",-1);
 }
 
 //バブルソート
